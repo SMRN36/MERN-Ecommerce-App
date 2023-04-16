240c0ffc09f40275691b911ca9c51a7574231f3e
simran bhandari <simranbhandari036@gmail.com>
2023-04-16 18:08:06 +0530

    created Register and Login API

---
+++ routes/authRoute.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {registerController, loginController} from '../controllers/authController.js'
+//router object
+const router = express.Router();
+
+//routing
+//register || method post
+router.post('/register', registerController);
+
+//login || method post
+router.post('/login', loginController);
+
+export default router;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
-
+import authRoutes from './routes/authRoute.js'
 //configure env
 dotenv.config();
 
@@ -17,17 +17,18 @@ const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
 
+//routes
+app.use('/api/v1/auth', authRoutes)
+
 //rest api
-app.get('/', (req, res) => {
-    res.send({
-        message : "Welcome"
-    })
-})
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to ecommerce app</h1>");
+  });
 
 //port
 const PORT = process.env.PORT || 8080;
 
 //run
 app.listen(PORT, () => {
-    console.log(`server running on ${PORT}`);
+    console.log(`server running on ${PORT}`.bgCyan.white);
 })
