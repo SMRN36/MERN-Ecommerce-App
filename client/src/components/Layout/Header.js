6b0bfcc10d9ee14de1ac59fcb241893cbc59be7b
simran bhandari <simranbhandari036@gmail.com>
2023-06-22 17:20:09 +0530

    private routes, location history, forgot password, and role base login

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -3,7 +3,6 @@ import { NavLink, Link } from "react-router-dom";
 import { useAuth } from "../../context/auth";
 import toast from "react-hot-toast";
 
-
 const Header = () => {
   const [auth, setAuth] = useAuth();
   const handleLogout = () => {
@@ -60,14 +59,32 @@ const Header = () => {
                 </>
               ) : (
                 <>
-                  <li className="nav-item">
+                  <li className="nav-item dropdown">
                     <NavLink
-                      onClick={handleLogout}
-                      to="/login"
-                      className="nav-link"
+                      className="nav-link dropdown-toggle"
+                      href="#"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
                     >
-                      Logout
+                      {auth?.user?.name}
                     </NavLink>
+                    <ul className="dropdown-menu">
+                      <li>
+                        <NavLink to="/dashboard" className="dropdown-item">
+                          Dashboard
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink
+                          onClick={handleLogout}
+                          to="/login"
+                          className="dropdown-item"
+                        >
+                          Logout
+                        </NavLink>
+                      </li>
+                    </ul>
                   </li>
                 </>
               )}
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,17 +1,31 @@
 import express from "express";
-import {registerController, loginController, testController,} from '../controllers/authController.js'
+import {
+  registerController,
+  loginController,
+  testController,
+  forgotPasswordController,
+} from "../controllers/authController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+
 //router object
 const router = express.Router();
 
 //routing
-//register || method post
-router.post('/register', registerController);
+//REGISTER || METHOD POST
+router.post("/register", registerController);
 
-//login || method post
-router.post('/login', loginController);
+//LOGIN || POST
+router.post("/login", loginController);
+
+//Forgot Password || POST
+router.post("/forgot-password", forgotPasswordController);
 
 //test routes
 router.get("/test", requireSignIn, isAdmin, testController);
+
+//protected route auth
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
 
 export default router;
