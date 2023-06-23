fa5af944fce4dbd3eedd6b3839308a2a4777189e
simran bhandari <simranbhandari036@gmail.com>
2023-06-23 12:27:29 +0530

    category API CRUD operations

--- server.js
+++ server.js
@@ -1,36 +1,42 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import colors from 'colors';
-import morgan from 'morgan';
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoute.js';
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 import cors from "cors";
+
 //configure env
 dotenv.config();
 
-//database config
+//databse config
 connectDB();
 
-//create rest object
-const app = express()
+//rest object
+const app = express();
 
-//middleware
+//middelwares
 app.use(cors());
-app.use(express.json())
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(morgan("dev"));
 
 //routes
-app.use('/api/v1/auth', authRoutes)
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
 
 //rest api
 app.get("/", (req, res) => {
-    res.send("<h1>Welcome to ecommerce app</h1>");
-  });
+  res.send("<h1>Welcome to ecommerce app</h1>");
+});
 
-//port
+//PORT
 const PORT = process.env.PORT || 8080;
 
-//run
+//run listen
 app.listen(PORT, () => {
-    console.log(`server running on ${PORT}`.bgCyan.white);
-})
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
+});
